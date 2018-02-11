e3d95a3e37f697bbae1c83f148a1a7c9ad00dd37
Erik Edin <erikedin@users.noreply.github.com>
2018-02-11 13:04:32 +0100

    add test for UTF-8 sequences in data frames

---
+++ test/data_frames_test.jl
@@ -0,0 +1,35 @@
+# These tests cover section 5.6 in the RFC6455 WebSocket specification.
+
+function textframe_from_server(text::String; final_frame=true)
+    Frame(final_frame, OPCODE_TEXT, false, length(text), 0, Vector{UInt8}(), Vector{UInt8}(text))
+end
+
+function continuation_textframe_from_server(text::String; final_frame=true)
+    Frame(final_frame, OPCODE_CONTINUATION, false, length(text), 0, Vector{UInt8}(), Vector{UInt8}(text))
+end
+
+@testset "Data frames            " begin
+    @testset "A text message fragment may include only a partial UTF-8 sequence" begin
+        # Tests that the client supports getting two text frames that are separately invalid UTF-8,
+        # but together form valid UTF-8.
+        #
+        # Requirement
+        # @5_6-1 Partial UTF-8 sequences
+
+        logic, handler, writer = makeclientlogic()
+
+        text = "\u2200"
+        # Split the above text into two parts that are both invalid UTF-8:
+        payload = Vector{UInt8}(text)
+        payload1 = payload[1:1]
+        payload2 = payload[2:end]
+
+        frame1 = Frame(false, OPCODE_TEXT, false, length(payload1), 0, Vector{UInt8}(), payload1)
+        frame2 = Frame(true, OPCODE_CONTINUATION, false, length(payload2), 0, Vector{UInt8}(), payload2)
+
+        handle(logic, FrameFromServer(frame1))
+        handle(logic, FrameFromServer(frame2))
+
+        @test gettextat(handler, 1) == text
+    end
+end
