6fa938550a46946058762089a804a35d6c687780
Erik Edin <erikedin@users.noreply.github.com>
2018-08-22 19:36:18 +0200

    handshake: implement more headers

--- src/handshake_logic.jl
+++ src/handshake_logic.jl
@@ -1,4 +1,5 @@
 using Random
+using Base64
 
 struct HTTPHandshakeLogic
     rng::AbstractRNG
@@ -7,4 +8,5 @@ end
 getrequestheaders(h::HTTPHandshakeLogic) = [
     "Sec-WebSocket-Version" => "13",
     "Upgrade" => "websocket",
-    "Connection" => "Upgrade"]
+    "Connection" => "Upgrade",
+    "Sec-WebSocket-Key" => base64encode(rand(h.rng, UInt8, 16))]
--- test/handshake_logic_test.jl
+++ test/handshake_logic_test.jl
@@ -1,4 +1,5 @@
 using DandelionWebSockets: HTTPHandshakeLogic, getrequestheaders
+using Base64
 
 @testset "Handshake logic        " begin
     @testset "Sec-WebSocket-Version; Version is 13" begin
@@ -29,5 +30,40 @@ using DandelionWebSockets: HTTPHandshakeLogic, getrequestheaders
         headersdict = Dict(headers)
 
         @test headersdict["Connection"] == "Upgrade"
+    end
+
+    @testset "Make two handshakes with different RNGs; Their Sec-WebSocket-Key are different" begin
+        rng1 = FakeRNG{UInt8}(b"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10")
+        h1 = HTTPHandshakeLogic(rng1)
+        rng2 = FakeRNG{UInt8}(b"\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x01")
+        h2 = HTTPHandshakeLogic(rng2)
+
+        headers1 = getrequestheaders(h1)
+        headersdict1 = Dict(headers1)
+        headers2 = getrequestheaders(h2)
+        headersdict2 = Dict(headers2)
+
+        @test headersdict1["Sec-WebSocket-Key"] != headersdict2["Sec-WebSocket-Key"]
+    end
+
+    @testset "Sec-WebSocket-Key; Is 16 bytes when base64 decoded" begin
+        rng = FakeRNG{UInt8}(b"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10")
+        h = HTTPHandshakeLogic(rng)
+
+        headers = getrequestheaders(h)
+        headersdict = Dict(headers)
+
+        @test length(base64decode(headersdict["Sec-WebSocket-Key"])) == 16
+    end
+
+    # This is an example Sec-WebSocket-Key from the specification, section 4.1.
+    @testset "Sec-WebSocket-Key uses nonce 0x01->0x10; Sec-WebSocket-Key is AQIDBAUGBwgJCgsMDQ4PEA==" begin
+        rng = FakeRNG{UInt8}(b"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10")
+        h = HTTPHandshakeLogic(rng)
+
+        headers = getrequestheaders(h)
+        headersdict = Dict(headers)
+
+        @test headersdict["Sec-WebSocket-Key"] == "AQIDBAUGBwgJCgsMDQ4PEA=="
     end
 end
