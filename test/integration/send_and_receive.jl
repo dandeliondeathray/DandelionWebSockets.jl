424c2d717764779108e1a95b029e2a67d0f48195
Erik Edin <erikedin@users.noreply.github.com>
2018-08-29 18:38:23 +0200

    integration: started sketching on another integration test

---
+++ test/integration/send_and_receive.jl
@@ -0,0 +1,68 @@
+"""
+"Send and receive" is an integration test that verifies that the client can send and receive
+messages.
+
+Five messages are sent from the client to the server, and five messages are sent from the server
+to the client.
+"""
+
+using DandelionWebSockets
+
+# These are the actions that the server will take.
+serverscript = [
+    WaitForOpen(),
+    SendTextFrame("1234"),
+    ShortWait(),
+    SendTextFrame("2345"),
+    ShortWait(),
+    SendTextFrame("3456"),
+    ShortWait(),
+    SendTextFrame("4567"),
+    ShortWait(),
+    SendTextFrame("5678"),
+    WaitForClose()
+]
+
+clientscript = [
+    WaitForOpen(),
+    SendTextFrame("abcd"),
+    ShortWait(),
+    SendTextFrame("bcde"),
+    ShortWait(),
+    SendTextFrame("cdef"),
+    ShortWait(),
+    SendTextFrame("defg"),
+    ShortWait(),
+    SendTextFrame("efgh"),
+    ShortWait(),
+    CloseConnection()
+]
+
+# Create server, and the handshake adapter that creates the connection when the client requests a
+# handshake.
+server = ScriptedServer(serverscript)
+handshakeadapter = InProcessHandshakeAdapter(server)
+
+# Create the WebSocket client, with a custom handshake adapter.
+# Instead of making an actual HTTP request, the custom handshake adapter creates an in-process
+# `IO` that is used in place of an actual network socket.
+# Note that the HTTP handshake logic is still the same, it's only the actual transport method that
+# is different.
+client = WSClient(; handshake=HTTPHandshake(RandomDevice(), handshakeadapter))
+
+# Create a client that runs the script above.
+clienthandler = ScriptedClientHandler(client, clientscript)
+
+# Request that the connection be opened, which starts the scripts for both server and client.
+wsconnect(client, "ws://the/uri/does/not/matter/here", sendandreceiveclient)
+
+# Wait for both client and server to close.
+waitforclose(clienthandler, server)
+
+@testset "Sent messages" begin
+    @test server.statistics.received_messages == 5
+end
+
+@testset "Received messages" begin
+    @test client.statistics.received_messages == 5
+end
