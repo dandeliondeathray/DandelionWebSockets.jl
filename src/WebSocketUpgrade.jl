a098f8f1d28cc618f10af92aee8d41fb20b92999
Erik Edin <erikedin@users.noreply.github.com>
2018-09-26 12:55:43 +0200

    websocketupgrade: finding the end of the response header

--- src/WebSocketUpgrade.jl
+++ src/WebSocketUpgrade.jl
@@ -2,9 +2,24 @@ module WebSocketUpgrade
 
 export ResponseParser, dataread, hascompleteresponse
 
-struct ResponseParser end
+function findfirstsubstring(needle::AbstractVector{UInt8}, haystack::AbstractVector{UInt8}) :: Union{Int, Nothing}
+    lastpossibleindex = length(haystack) - length(needle) + 1
+    for i = 1:lastpossibleindex
+        substring = haystack[i:i + length(needle) - 1]
+        if substring == needle
+            return i
+        end
+    end
+    nothing
+end
 
-dataread(::ResponseParser, ::AbstractVector{UInt8}) = nothing
-hascompleteresponse(::ResponseParser) = true
+struct ResponseParser
+    data::Vector{UInt8}
+
+    ResponseParser() = new(Vector{UInt8}())
+end
+
+dataread(parser::ResponseParser, data::AbstractVector{UInt8}) = append!(parser.data, data)
+hascompleteresponse(parser::ResponseParser) = findfirstsubstring(b"\r\n\r\n", parser.data) != nothing
 
 end
--- test/WebSocketUpgrade/WebSocketUpgrade_test.jl
+++ test/WebSocketUpgrade/WebSocketUpgrade_test.jl
@@ -21,4 +21,78 @@ todata(xs...) = codeunits(join(xs))
         @test hascompleteresponse(parser)
     end
 
+    @testset "ResponseParser; No empty line; The parser does not have a complete response" begin
+        # Arrange
+        response = todata(
+            "HTTP/1.1 101 Switch Protocols\r\n",
+            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+        )
+
+        parser = ResponseParser()
+
+        # Act
+        dataread(parser, response)
+
+        # Assert
+        @test !hascompleteresponse(parser)
+    end
+
+    @testset "ResponseParser; A complete response with some excess data; The parser has a complete response" begin
+        # Arrange
+        response = todata(
+            "HTTP/1.1 101 Switch Protocols\r\n",
+            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+            "\r\n",
+            "Some excess data"
+        )
+
+        parser = ResponseParser()
+
+        # Act
+        dataread(parser, response)
+
+        # Assert
+        @test hascompleteresponse(parser)
+    end
+
+    @testset "ResponseParser; Complete response in two parts; The parser has a complete response" begin
+        # Arrange
+        response1 = todata(
+            "HTTP/1.1 101 Switch Protocols\r\n",
+        )
+        response2 = todata(
+            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+            "\r\n",
+        )
+
+        parser = ResponseParser()
+
+        # Act
+        dataread(parser, response1)
+        dataread(parser, response2)
+
+        # Assert
+        @test hascompleteresponse(parser)
+    end
+
+    @testset "ResponseParser; Complete response in two parts with excess; The parser has a complete response" begin
+        # Arrange
+        response1 = todata(
+            "HTTP/1.1 101 Switch Protocols\r\n",
+        )
+        response2 = todata(
+            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+            "\r\n",
+            "Some excess data"
+        )
+
+        parser = ResponseParser()
+
+        # Act
+        dataread(parser, response1)
+        dataread(parser, response2)
+
+        # Assert
+        @test hascompleteresponse(parser)
+    end
 end
