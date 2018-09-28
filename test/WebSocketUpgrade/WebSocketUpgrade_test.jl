3471d24de8b1263e7bbac53624bdff9ad6b9daeb
Erik Edin <erikedin@users.noreply.github.com>
2018-09-28 17:13:34 +0200

    websocketupgrade: categorize tests

--- test/WebSocketUpgrade/WebSocketUpgrade_test.jl
+++ test/WebSocketUpgrade/WebSocketUpgrade_test.jl
@@ -5,167 +5,174 @@ using DandelionWebSockets.WebSocketUpgrade: BadHTTPResponse
 todata(xs...) = codeunits(join(xs))
 
 @testset "WebSocketUpgrade       " begin
-    @testset "ResponseParser; A complete HTTP response without excess; The parser has a complete HTTP response" begin
-        # Arrange
-        response = todata(
-            "HTTP/1.1 101 Switch Protocols\r\n",
-            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
-            "\r\n",
-        )
+    @testset "Header boundary" begin
+        @testset "ResponseParser; A complete HTTP response without excess; The parser has a complete HTTP response" begin
+            # Arrange
+            response = todata(
+                "HTTP/1.1 101 Switch Protocols\r\n",
+                "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+                "\r\n",
+            )
 
-        parser = ResponseParser()
+            parser = ResponseParser()
 
-        # Act
-        dataread(parser, response)
+            # Act
+            dataread(parser, response)
 
-        # Assert
-        @test hascompleteresponse(parser)
+            # Assert
+            @test hascompleteresponse(parser)
+        end
+
+        @testset "ResponseParser; No empty line; The parser does not have a complete response" begin
+            # Arrange
+            response = todata(
+                "HTTP/1.1 101 Switch Protocols\r\n",
+                "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+            )
+
+            parser = ResponseParser()
+
+            # Act
+            dataread(parser, response)
+
+            # Assert
+            @test !hascompleteresponse(parser)
+        end
+
+        @testset "ResponseParser; A complete response with some excess data; The parser has a complete response" begin
+            # Arrange
+            response = todata(
+                "HTTP/1.1 101 Switch Protocols\r\n",
+                "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+                "\r\n",
+                "Some excess data"
+            )
+
+            parser = ResponseParser()
+
+            # Act
+            dataread(parser, response)
+
+            # Assert
+            @test hascompleteresponse(parser)
+        end
+
+        @testset "ResponseParser; Complete response in two parts; The parser has a complete response" begin
+            # Arrange
+            response1 = todata(
+                "HTTP/1.1 101 Switch Protocols\r\n",
+            )
+            response2 = todata(
+                "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+                "\r\n",
+            )
+
+            parser = ResponseParser()
+
+            # Act
+            dataread(parser, response1)
+            dataread(parser, response2)
+
+            # Assert
+            @test hascompleteresponse(parser)
+        end
+
+        @testset "ResponseParser; Complete response in two parts with excess; The parser has a complete response" begin
+            # Arrange
+            response1 = todata(
+                "HTTP/1.1 101 Switch Protocols\r\n",
+            )
+            response2 = todata(
+                "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+                "\r\n",
+                "Some excess data"
+            )
+
+            parser = ResponseParser()
+
+            # Act
+            dataread(parser, response1)
+            dataread(parser, response2)
+
+            # Assert
+            @test hascompleteresponse(parser)
+        end
     end
 
-    @testset "ResponseParser; No empty line; The parser does not have a complete response" begin
-        # Arrange
-        response = todata(
-            "HTTP/1.1 101 Switch Protocols\r\n",
-            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
-        )
+    @testset "Status Line" begin
+        @testset "ResponseParser; Response with status code 101; Parse result has status 101" begin
+            # Arrange
+            responsetext = todata(
+                "HTTP/1.1 101 Switch Protocols\r\n",
+                "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+                "\r\n",
+            )
 
-        parser = ResponseParser()
+            parser = ResponseParser()
+            dataread(parser, responsetext)
 
-        # Act
-        dataread(parser, response)
+            # Act
+            response = parseresponse(parser)
 
-        # Assert
-        @test !hascompleteresponse(parser)
+            # Assert
+            @test response.status == 101
+        end
+
+        @testset "ResponseParser; Response with status code 200; Parse result has status 200" begin
+            # Arrange
+            responsetext = todata(
+                "HTTP/1.1 200 OK\r\n",
+                "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+                "\r\n",
+            )
+
+            parser = ResponseParser()
+            dataread(parser, responsetext)
+
+            # Act
+            response = parseresponse(parser)
+
+            # Assert
+            @test response.status == 200
+        end
+
+
+        @testset "ResponseParser; Status code is 101 and year is 200x; Parse result has status 101" begin
+            # Arrange
+            responsetext = todata(
+                "HTTP/1.1 101 Switch Protocols\r\n",
+                "Date: Sun, 06 Nov 2008 08:49:37 GMT\r\n",
+                "\r\n",
+            )
+
+            parser = ResponseParser()
+            dataread(parser, responsetext)
+
+            # Act
+            response = parseresponse(parser)
+
+            # Assert
+            @test response.status == 101
+        end
+
+        # TODO: Test spacing in the status line
     end
 
-    @testset "ResponseParser; A complete response with some excess data; The parser has a complete response" begin
-        # Arrange
-        response = todata(
-            "HTTP/1.1 101 Switch Protocols\r\n",
-            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
-            "\r\n",
-            "Some excess data"
-        )
+    @testset "Malformed Status line" begin
+        @testset "ResponseParser; Malformed Status Line; BadHTTPResponse is thrown" begin
+            # Arrange
+            responsetext = todata(
+                "Malformed HTTP header",
+                "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
+                "\r\n",
+            )
 
-        parser = ResponseParser()
+            parser = ResponseParser()
+            dataread(parser, responsetext)
 
-        # Act
-        dataread(parser, response)
-
-        # Assert
-        @test hascompleteresponse(parser)
+            # Act
+            @test_throws BadHTTPResponse parseresponse(parser)
+        end
     end
 
-    @testset "ResponseParser; Complete response in two parts; The parser has a complete response" begin
-        # Arrange
-        response1 = todata(
-            "HTTP/1.1 101 Switch Protocols\r\n",
-        )
-        response2 = todata(
-            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
-            "\r\n",
-        )
-
-        parser = ResponseParser()
-
-        # Act
-        dataread(parser, response1)
-        dataread(parser, response2)
-
-        # Assert
-        @test hascompleteresponse(parser)
-    end
-
-    @testset "ResponseParser; Complete response in two parts with excess; The parser has a complete response" begin
-        # Arrange
-        response1 = todata(
-            "HTTP/1.1 101 Switch Protocols\r\n",
-        )
-        response2 = todata(
-            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
-            "\r\n",
-            "Some excess data"
-        )
-
-        parser = ResponseParser()
-
-        # Act
-        dataread(parser, response1)
-        dataread(parser, response2)
-
-        # Assert
-        @test hascompleteresponse(parser)
-    end
-
-    @testset "ResponseParser; Response with status code 101; Parse result has status 101" begin
-        # Arrange
-        responsetext = todata(
-            "HTTP/1.1 101 Switch Protocols\r\n",
-            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
-            "\r\n",
-        )
-
-        parser = ResponseParser()
-        dataread(parser, responsetext)
-
-        # Act
-        response = parseresponse(parser)
-
-        # Assert
-        @test response.status == 101
-    end
-
-    @testset "ResponseParser; Response with status code 200; Parse result has status 200" begin
-        # Arrange
-        responsetext = todata(
-            "HTTP/1.1 200 OK\r\n",
-            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
-            "\r\n",
-        )
-
-        parser = ResponseParser()
-        dataread(parser, responsetext)
-
-        # Act
-        response = parseresponse(parser)
-
-        # Assert
-        @test response.status == 200
-    end
-
-
-    @testset "ResponseParser; Status code is 101 and year is 200x; Parse result has status 101" begin
-        # Arrange
-        responsetext = todata(
-            "HTTP/1.1 101 Switch Protocols\r\n",
-            "Date: Sun, 06 Nov 2008 08:49:37 GMT\r\n",
-            "\r\n",
-        )
-
-        parser = ResponseParser()
-        dataread(parser, responsetext)
-
-        # Act
-        response = parseresponse(parser)
-
-        # Assert
-        @test response.status == 101
-    end
-
-    # TODO: Test spacing in the status line
-
-    @testset "ResponseParser; Malformed Status Line; BadHTTPResponse is thrown" begin
-        # Arrange
-        responsetext = todata(
-            "Malformed HTTP header",
-            "Date: Sun, 06 Nov 1994 08:49:37 GMT\r\n",
-            "\r\n",
-        )
-
-        parser = ResponseParser()
-        dataread(parser, responsetext)
-
-        # Act
-        @test_throws BadHTTPResponse parseresponse(parser)
-    end
 end
